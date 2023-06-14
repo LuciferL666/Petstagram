aaf140757c54b67e23bef03837c46299ba242566
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-15 00:00:18 +0200

    Config extension, config views folder, add main layout

--- InfoFile.md
+++ InfoFile.md
@@ -7,10 +7,17 @@
  - npm i -D nodemon
 3. Install and setup express
  - npm i express
- * B Add static middleware
- * C Add body parser
- * D Add routes
+ * B, Add static middleware
+ * C, Add body parser
+ * D, Add routes
 4. Add static resources 
  - create public folder put inside css, images
 5. Add views folder with ready htmls
  - create views folder put inside htmls files
+6. Add express-handlebars view engine
+ - npm i express-handlebars
+ * Install
+ * A, Add to express
+ * B, Config extension
+ * C, Config views folder (only if src is used)
+ * D, Create folder layout with file main for main parts of html 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-// 3. A
-const express = require('express');
+const express = require('express');// 3. A
+const handlebars = require('express-handlebars');// 6. A
 
 const routes = require('./routes') // 3 D
 
-const app = express();
+const app = express(); // 3. A
+
+app.engine('hbs', handlebars.engine({ // 6. A
+    extname: 'hbs'  //6. B
+}));
+app.set('view engine', 'hbs'); //6. B
+app.set('views', 'src/views'); //6. C
 
 app.use(express.static('public')) //B
 app.use(express.urlencoded({extended: false})) //C ?
 
 
-app.use(routes)
+app.use(routes)// 3. A
 
-app.listen(3000, console.log(`Server is listening on port 3000...`));
+app.listen(3000, console.log(`Server is listening on port 3000...`));// 3. A
