ad602ab97badcc93abd96b40757ea13705e6bf0d
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-14 23:12:40 +0200

    Setup express

--- InfoFile.md
+++ InfoFile.md
@@ -3,5 +3,7 @@
 1. Initialize project and structure
  - npm init --yes
  - making folder src and inside making file index.js
- 2. Setup dev environment
+2. Setup dev environment
  - npm i -D nodemon
+3. Install and setup express
+ - npm i express
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Server start');
+// 3. A
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) =>{
+    res.send('first action')
+});
+
+app.listen(3000, console.log(`Server is listening on port 3000...`));
