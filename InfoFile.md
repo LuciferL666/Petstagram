c4a78a1a594d5725c54d8b21920392e57aee52c7
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-15 21:47:12 +0200

    Add and connect database

--- InfoFile.md
+++ InfoFile.md
@@ -16,7 +16,7 @@
  - create views folder put inside htmls files
 6. Add express-handlebars view engine
  - npm i express-handlebars
- * Install
+ * Install express
  * A, Add to express
  * B, Config extension
  * C, Config views folder (only if src is used)
@@ -26,3 +26,6 @@
  * F, Fix static paths (to fix it can rename public folder to static or remove static in main.hbs from: src="/static/images/ and from: /static/css/styles.css)
 7. A, Add controllers folder with home controller(create Controllers folder in src with file home Controller) 
  - B, Moving render home from routers file to homeController file
+8. Add database 
+ * Install mongoose (npm i mongoose)
+ * A, Connect data base (IN INDEX)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');// 3. A
 const handlebars = require('express-handlebars');// 6. A
 const path = require('path')// 6. F
+const mongoose = require('mongoose') //8. A
 
 const routes = require('./routes') // 3. D
 
 const app = express(); // 3. A
+
+//TODO CHANGE DB NAME
+mongoose.connect(`mongodb://localhost:27017/Petstagram`)
+.then(() => console.log('DB connected successfully'))
+.catch(err => console.log('DB Error, ', err.message));
+
 
 app.engine('hbs', handlebars.engine({ // 6. A
     extname: 'hbs'  //6. B
@@ -12,10 +19,9 @@ app.engine('hbs', handlebars.engine({ // 6. A
 app.set('view engine', 'hbs'); //6. B
 app.set('views', 'src/views'); //6. C
 
+
 app.use(express.static(path.resolve(__dirname, 'public'))) //3. B
 app.use(express.urlencoded({extended: false})) // 3. C ?
-
-
 app.use(routes)// 3. D
 
 app.listen(3000, console.log(`Server is listening on port 3000...`));// 3. A
