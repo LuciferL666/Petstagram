a1f42cab3e1b2ae8fbf718623ccf406c9304f8ed
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-16 20:13:56 +0200

    Login after register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ const router = require('express').Router(); //9. A
 
 const userManager = require('../managers/userManager'); //11. B
 const { TOKEN_KEY } =  require('../config/config') // 20. F
+const {getErrorMessage} =  require('../utils/errorHelpers'); //24. D
+
 
 router.get('/login', (req, res) =>{//9. A
 res.render('users/login') //9. C
@@ -10,12 +12,17 @@ res.render('users/login') //9. C
 router.post('/login', async (req, res) =>{ //13. A
     const { username, password } =  req.body; //13. A
 
-    const token =  await userManager.login(username, password); //13. A
-      //17. B(const token)
+    try {
+        const token =  await userManager.login(username, password); //13. A
+        //17. B(const token)
+  
+        res.cookie(TOKEN_KEY, token); //18. B //20. F
+  
+      res.redirect('/'); //18. B
+    } catch (err){
+        res.render('users/login', {error: getErrorMessage(err)})
+    }
 
-      res.cookie(TOKEN_KEY, token); //18. B //20. F
-
-    res.redirect('/'); //18. B
 }) //13. A
 
 router.get('/register', (req, res)=>{ //9. D
@@ -25,10 +32,15 @@ router.get('/register', (req, res)=>{ //9. D
 router.post('/register', async(req, res)=>{ //13. A
     const {username, email, password, repeatPassword} = req.body //13. A
 
-    await userManager.register({username, email, password, repeatPassword}); //13. A
-
-    res.redirect('/users/login');  //18. B befor res.send(registered)
-})
+    try { //24. D
+        const token = await userManager.register({username, email, password, repeatPassword}); //13. A //25. A( add const token)
+        
+        res.cookie(TOKEN_KEY, token)//25. A
+        res.redirect('/');  //18. B befor res.send(registered) //25. A('/')
+    }catch (err) { //24. D
+res.render('users/register', { error: getErrorMessage(err) }); //24. D
+    }
+});
 
 router.get('/logout', (req, res)=>{ //19. A
     res.clearCookie('token'); //19. A
