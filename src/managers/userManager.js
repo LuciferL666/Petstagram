557095d9150a565a54e95790c9963b23d05a7a25
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-16 10:15:22 +0200

    Setup login  with validate password and user

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,8 +1,27 @@
 const User = require('../models/User'); //11. A
 
-exports.login = (username, password) => { //11. A
-                  //14. A
+const bcrypt = require('bcrypt'); //16. B
+
+exports.login = async (username, password) => { //11. A
+                          //14. A
+    //find user by username
+const user = await User.findOne({ username }); //16. A
+if(!user){
+    throw new Error('Invalid user or password'); //16. A
+}; //16. A
+    //check password
+
+const isValid = await bcrypt.compare(password, user.password); //16. B
+if(!isValid){ //16. B
+    throw new Error('Invalid user or password'); //16. B
+}; //16. B
 }; //11. A
                     
-exports.register = (userData) => User.create(userData) //11. A
-                              //14. A
+exports.register = async (userData) => {//11. A
+const user = await User.findOne({username: userData. username}); //14. B
+if(user){//10. C
+    throw new Error('Username already exists')//14. B
+}//14. B
+return User.create(userData) //14. B
+}//14. B
+                              
